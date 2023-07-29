592c2a00a890565ce824ec2d1814734d4d9a036d
Vitali <terrmit574@gmail.com>
2023-07-29 15:38:43 +0300

    feat: add change input value

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector(".search-model");
 const modalBtn = document.querySelector(".icon_search");
 const modalClose = modal.querySelector(".search-close-switch");
+const searchInput = modal.querySelector("#search-input");
 
 modalBtn.addEventListener("click", () => {
   modal.style.display = "block";
@@ -8,4 +9,8 @@ modalBtn.addEventListener("click", () => {
 
 modalClose.addEventListener("click", () => {
   modal.style.display = "none";
+});
+
+searchInput.addEventListener("input", () => {
+  console.log(searchInput.value);
 });
