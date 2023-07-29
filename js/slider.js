98887dc4cfa326a3350015b1d59981a724d70199
Vitali <terrmit574@gmail.com>
2023-07-29 20:07:17 +0300

    ref: add encapsulation to JS files

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,16 @@
-const swiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
+const slider = () => {
+  const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  effect: "fade",
-  speed: 1000,
-});
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    effect: "fade",
+    speed: 1000,
+  });
+};
+
+slider();
